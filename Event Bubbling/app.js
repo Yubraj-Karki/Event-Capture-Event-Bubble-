93b2b836b2ec6fa912afd620757f2efe0c4722ad
YubrajKarki <yubrajkarki528@gmail.com>
2021-08-15 20:43:03 +0545

    Managed folder structures and changed documents' names

--- Event Bubbling/app.js
+++ Event Bubbling/app.js
@@ -5,19 +5,31 @@ const child = document.querySelector("#child");
 const secondChild = document.querySelector("#secondChild");
 
 // Event Listeners
-child.addEventListener("click", function () {
-  console.log("children clicked");
-});
-parent.addEventListener("click", function () {
-  console.log("parent clicked");
-});
-main.addEventListener("click", function () {
-  console.log("main clicked");
-});
-secondChild.addEventListener("click", function () {
-  console.log("second child clicked");
-});
-
-// When you click on the children button or the second children button,
-// first it fires it's event then it fires the ancestors' events
-// all the way up in a dom tree till the outermost DOM element, which is event bubbling
+child.addEventListener(
+  "click",
+  function () {
+    console.log("children clicked");
+  },
+  true
+);
+parent.addEventListener(
+  "click",
+  function () {
+    console.log("parent clicked");
+  },
+  true
+);
+main.addEventListener(
+  "click",
+  function () {
+    console.log("main clicked");
+  },
+  true
+);
+secondChild.addEventListener(
+  "click",
+  function () {
+    console.log("second child clicked");
+  },
+  true
+);
