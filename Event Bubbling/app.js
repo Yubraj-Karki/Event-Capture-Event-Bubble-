2e002cf8795d411c5446a5e8bd0d3995eea1142e
YubrajKarki <yubrajkarki528@gmail.com>
2021-08-15 20:49:46 +0545

    Written event capturing codes

--- Event Bubbling/app.js
+++ Event Bubbling/app.js
@@ -5,31 +5,15 @@ const child = document.querySelector("#child");
 const secondChild = document.querySelector("#secondChild");
 
 // Event Listeners
-child.addEventListener(
-  "click",
-  function () {
-    console.log("children clicked");
-  },
-  true
-);
-parent.addEventListener(
-  "click",
-  function () {
-    console.log("parent clicked");
-  },
-  true
-);
-main.addEventListener(
-  "click",
-  function () {
-    console.log("main clicked");
-  },
-  true
-);
-secondChild.addEventListener(
-  "click",
-  function () {
-    console.log("second child clicked");
-  },
-  true
-);
+child.addEventListener("click", function () {
+  console.log("children clicked");
+});
+parent.addEventListener("click", function () {
+  console.log("parent clicked");
+});
+main.addEventListener("click", function () {
+  console.log("main clicked");
+});
+secondChild.addEventListener("click", function () {
+  console.log("second child clicked");
+});
--- Event Capturing/app.js
+++ Event Capturing/app.js
@@ -5,18 +5,34 @@ const child = document.querySelector("#child");
 const secondChild = document.querySelector("#secondChild");
 
 // Event Listeners
-child.addEventListener("click", function () {
-  console.log("children clicked");
-});
-parent.addEventListener("click", function () {
-  console.log("parent clicked");
-});
-main.addEventListener("click", function () {
-  console.log("main clicked");
-});
-secondChild.addEventListener("click", function () {
-  console.log("second child clicked");
-});
+child.addEventListener(
+  "click",
+  function () {
+    console.log("children clicked");
+  },
+  true
+);
+parent.addEventListener(
+  "click",
+  function () {
+    console.log("parent clicked");
+  },
+  true
+);
+main.addEventListener(
+  "click",
+  function () {
+    console.log("main clicked");
+  },
+  true
+);
+secondChild.addEventListener(
+  "click",
+  function () {
+    console.log("second child clicked");
+  },
+  true
+);
 
 // When you click on the children button or the second children button,
 // first it fires it's event then it fires the ancestors' events
