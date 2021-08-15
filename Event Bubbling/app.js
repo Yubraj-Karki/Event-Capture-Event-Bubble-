cb54f1d981696f0c8d1b0446581ad9262c4cf01a
YubrajKarki <yubrajkarki528@gmail.com>
2021-08-15 20:38:14 +0545

    change folder structure and completed event bubbling

---
+++ Event Bubbling/app.js
@@ -0,0 +1,23 @@
+// Grabbing elements
+const parent = document.querySelector("#parent");
+const main = document.querySelector("#main");
+const child = document.querySelector("#child");
+const secondChild = document.querySelector("#secondChild");
+
+// Event Listeners
+child.addEventListener("click", function () {
+  console.log("children clicked");
+});
+parent.addEventListener("click", function () {
+  console.log("parent clicked");
+});
+main.addEventListener("click", function () {
+  console.log("main clicked");
+});
+secondChild.addEventListener("click", function () {
+  console.log("second child clicked");
+});
+
+// When you click on the children button or the second children button,
+// first it fires it's event then it fires the ancestors' events
+// all the way up in a dom tree till the outermost DOM element, which is event bubbling
